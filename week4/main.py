2a9616c9a8b4cc36678e27a9331d883b6d1b2308
finn20240315 <163500451+finn20240315@users.noreply.github.com>
2025-02-09 12:35:49 +0800

    week4_20250209

---
+++ week4/main.py
@@ -0,0 +1,80 @@
+from fastapi import FastAPI, Form, Request
+from fastapi.responses import HTMLResponse, Response, RedirectResponse, FileResponse
+from starlette.middleware.base import BaseHTTPMiddleware
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+app=FastAPI()
+
+templates = Jinja2Templates(directory="templates")
+
+class myMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request, call_next):
+        print(f"被攔截的請求: {request.method} {request.url}")
+        
+        logined = request.cookies.get("already_login")
+        if request.url.path.startswith("/member") and logined != "true":
+            return RedirectResponse("/",status_code=302)
+        
+           # 繼續執行後續請求
+        response = await call_next(request)
+        return response
+    
+app.add_middleware(myMiddleware)
+
+@app.get("/")
+def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.post("/signin")
+async def signin(username: str = Form(...), password: str = Form(...)):
+    if username == "test" and password == "test":  # 這裡可以改成資料庫驗證
+        response = RedirectResponse(url="/member", status_code=302)
+        response.set_cookie(key="already_login", value="true")  # 設置登入 Cookie
+        return response
+    return RedirectResponse(url="/error?message=登入失敗，請重新輸入", status_code=302)
+
+@app.get("/member", response_class=HTMLResponse)
+async def member():
+    return HTMLResponse("""<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>member</title>
+    <link rel="stylesheet" href="/static/style.css" />
+  </head>
+  <body>
+    <h2>歡迎光臨，這是會員頁</h2>
+    <h4>恭喜您，成功登入系統</h4>
+    <a href="/signout">登出系統</a>
+  </body>
+</html>""")
+
+@app.get("/signout")
+async def logout():
+    response = RedirectResponse(url="/", status_code=302)
+    response.delete_cookie("already_login")  # 刪除登入 Cookie
+    return response
+
+@app.get("/error")
+async def error(message: str):
+    return HTMLResponse(f"""
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>錯誤頁面</title>
+        <link rel="stylesheet" href="/static/style.css">
+    </head>
+    <body>
+        <h2>失敗頁面</h2>
+        <h4>{message}</h4>
+        <a href="/">回到首頁</a>
+    </body>
+    </html>
+    """)
+
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
